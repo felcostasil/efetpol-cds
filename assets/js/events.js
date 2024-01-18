16db6a926e82f81cc4c53daff73fe393fe1fd1a9
Felipe Costa Silva <fcsfelipesilva@gmail.com>
2024-01-18 17:32:34 -0300

    Pagination development

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,13 +1,19 @@
-import { getURI, getData } from "./api.js"
+import { getURI, getData, getNextUri, getPreviousUri } from "./api.js"
 import { handleDisplay } from "./display.js"
-
 
 export const search = async function search() {
   const field = getInputValue()
   if (!field) throw new Error("Nothing was selected")
-
+  const previousUrl = getPreviousUri(field)
+  const nextUrl = getNextUri(field)
   const url = getURI(field)
+  
+  console.log({previousUrl, nextUrl, url})
+
   const data = await getData(url)
+  console.log(data)
+  console.log(field)
+
 
 
   showData(field.key, data)
@@ -50,6 +56,35 @@ export const clickRow = async function (event) {
   showData("matricula", data)
 }
 
+export const nextPage = async function nextPage() {
+
+  window.currentPage++
+  if (window.currentPage > 12) {
+    window.currentPage =12
+    return
+  }
+  const url = getURI({key: "nome", value: window.searchName})
+  
+  const data = await getData(url)
+  console.log(data)
+  
+  showData("nome", data)
+}
+
+export const backPage = async function backPage() {
+  window.currentPage--
+  if (window.currentPage < 1) {
+    window.currentPage = 1
+    return
+  }
+  const url = getURI({key: "nome", value: window.searchName})
+  
+  const data = await getData(url)
+  console.log(data)
+
+  showData("nome", data)
+}
+
 const getInputValue = function () {
   const inputs = ["nome", "matricula"]
   let field
@@ -57,7 +92,9 @@ const getInputValue = function () {
   for (const input of inputs) {
     const elem = document.querySelector(`#${input}`)
     if (!elem) continue
+    window.searchName = elem.value
     field = { key: input, value: elem.value }
+    console.log(field)
     break;
   }
   return field
